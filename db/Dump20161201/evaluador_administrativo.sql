6388e12234e07b2e842a596dd8683f6bca5ba206
fabianLeon <fabianLeon@localhost.localdomain>
2016-12-01 23:05:17 -0500

    inclusion de datos para la generacion de preguntas cerradas y abiertas

---
+++ db/Dump20161201/evaluador_administrativo.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.7.13, for linux-glibc2.5 (x86_64)
+--
+-- Host: 127.0.0.1    Database: evaluador
+-- ------------------------------------------------------
+-- Server version	5.5.50-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrativo`
+--
+
+DROP TABLE IF EXISTS `administrativo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrativo` (
+  `pk_administrativo` varchar(13) NOT NULL,
+  `n_nombres` varchar(100) DEFAULT NULL,
+  `n_apellido1` varchar(50) DEFAULT NULL,
+  `n_apellido2` varchar(50) DEFAULT NULL,
+  `n_correo` varchar(70) DEFAULT NULL,
+  `p_contrasena` varchar(20) DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`pk_administrativo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrativo`
+--
+
+LOCK TABLES `administrativo` WRITE;
+/*!40000 ALTER TABLE `administrativo` DISABLE KEYS */;
+INSERT INTO `administrativo` VALUES ('1013611972','Fabián','Sánchez','Léon','fabi_leon@outlook.com','ABcd1234','Ingeniero');
+/*!40000 ALTER TABLE `administrativo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-01 23:02:11
